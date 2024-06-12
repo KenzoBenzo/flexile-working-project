0761ad7b241f9e3daa513ec7c7967a73f3fb51f2
KenzoBenzo <32865577+KenzoBenzo@users.noreply.github.com>
2024-06-12 12:29:34 -0400

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	devtools: { enabled: true },
+	css: ["~/assets/css/main.css"],
+	modules: ["@vueuse/motion/nuxt"],
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	// animations
+	runtimeConfig: {
+		public: {
+			motion: {
+				directives: {
+					"pop-bottom": {
+						initial: {
+							scale: 0,
+							opacity: 0,
+							y: 100,
+						},
+						visible: {
+							scale: 1,
+							opacity: 1,
+							y: 0,
+						},
+					},
+				},
+			},
+		},
+	},
+});
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+	<div>
+		<h1 class="text-3xl font-bold underline">Hello world!</h1>
+		<p>Current route: {{ route.path }}</p>
+		<a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
+			>Learn more about Nuxt Routing</a
+		>
+		<div v-motion-pop-bottom class="h-8 w-8 rounded bg-blue-500" />
+	</div>
+</template>
